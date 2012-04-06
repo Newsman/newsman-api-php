4efe30bbc7daf0d685848e79b769c2c424b2ae03
Alex <alex@dazoot.ro>
2012-04-06 14:24:01 +0300

    import csv example

--- example/import/csv.php
+++ example/import/csv.php
@@ -1,5 +1,5 @@
 <?php
-// @see: http://kb.newsman.ro/api/1.2/template.mailTemplateAdd
+// @see: http://kb.newsman.ro/api/1.2/import.csv
 // you can get your API KEY and your newsman_user_id from My account -> API
 require_once("Newsman/Client.php");
 
