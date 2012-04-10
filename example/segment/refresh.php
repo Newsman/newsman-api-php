6026ea04c16811c70abc8d298173de0e45d75b48
Alex <alex@dazoot.ro>
2012-04-10 12:37:11 +0300

    newsman api new examples

---
+++ example/segment/refresh.php
@@ -0,0 +1,14 @@
+<?php
+// @see: http://kb.newsman.ro/api/1.2/segment.refresh
+// you can get your API KEY and your newsman_user_id from My account -> API
+require_once("Newsman/Client.php");
+
+$client = new Newsman_Client($newsman_user_id, $api_key);
+$ret = $client->segment->refresh($segment_id);
+
+if ($ret !== true)
+{
+	// handle error here
+	die("Error on method segment.refresh");
+}
+?>
