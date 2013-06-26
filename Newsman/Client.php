c847b33b28567fb1dd8a5381f384d91898c96ef1
dazoot <catalin@dazoot.ro>
2013-06-26 16:06:11 +0300

    typo fix

--- Newsman/Client.php
+++ Newsman/Client.php
@@ -245,7 +245,7 @@ class Newsman_Client
 			if (is_numeric($v) || is_string($v)) $ret[$k] = $v.""; // always string
 			elseif ($v instanceof DateTime)
 			{
-				$v = $v->format(DateTime::ISO8601);
+				$ret[$k] = $v->format(DateTime::ISO8601);
 			}
 			else $ret[$k] = json_encode($v);
 		}
